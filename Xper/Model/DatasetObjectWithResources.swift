3906b02a551e49ce87e323b458e1e2f3d940cc1b
Thomas Burguiere <thomasburguiere@users.noreply.github.com>
2016-04-05 09:11:50 +0200

    using protocol and extension for DatasetObjectWithResources

--- Xper/Model/DatasetObjectWithResources.swift
+++ Xper/Model/DatasetObjectWithResources.swift
@@ -8,14 +8,16 @@
 
 import Foundation
 
-class DatasetObjectWithResources {
-    var resources: [Resource]  = []
-    func addResource(addedResource: Resource) {
+protocol DatasetObjectWithResources {
+    var resources: [Resource]  {get set}
+}
+extension DatasetObjectWithResources {
+   mutating func addResource(addedResource: Resource) {
         resources.append(addedResource)
         addedResource.object = self
     }
     
-    func removeResource(deletedResource: Resource) {
+    mutating func removeResource(deletedResource: Resource) {
         resources = resources.filter({ (resource: Resource) -> Bool in
             if(resource === deletedResource){
                 deletedResource.object = nil
@@ -26,7 +28,7 @@ class DatasetObjectWithResources {
         })
     }
     
-    func deleteAllResources() {
+    mutating func deleteAllResources() {
         for resource in resources {
             resource.object = nil
         }
