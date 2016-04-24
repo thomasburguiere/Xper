f1fbeb7e2acc5906f5f93cad357248b18d708fab
Thomas Burguiere <thomas.burguiere@gmail.com>
2016-04-24 20:25:26 +0200

    added framework to source control

---
+++ Carthage/Build/iOS/XperFramework.framework/Headers/XperFramework.h
@@ -0,0 +1,19 @@
+//
+//  XperFramework.h
+//  XperFramework
+//
+//  Created by Thomas Burguiere on 09/04/16.
+//  Copyright © 2016 Thomas Burguiere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XperFramework.
+FOUNDATION_EXPORT double XperFrameworkVersionNumber;
+
+//! Project version string for XperFramework.
+FOUNDATION_EXPORT const unsigned char XperFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XperFramework/PublicHeader.h>
+
+
