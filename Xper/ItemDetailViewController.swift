fed294ba76eebe31d7d0a786eeabe0057d02f820
Thomas Burguiere <thomas.burguiere@gmail.com>
2016-05-02 05:31:02 +0200

    added extension to preserve font settings in UITextView containing attributed text

--- Xper/ItemDetailViewController.swift
+++ Xper/ItemDetailViewController.swift
@@ -34,17 +34,28 @@ class ItemDetailViewController: UIViewController, UINavigationControllerDelegate
         if let item = item {
             navigationItem.title = item.name
             let detailString = item.detail
-            do {
-                let formatted = try NSAttributedString(
-                    data: (detailString?.dataUsingEncoding(NSUnicodeStringEncoding))!,
-                    options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType],
-                    documentAttributes: nil)
-                itemDetailText.attributedText = formatted
-            } catch {
-                itemDetailText.text = detailString
-            }
+            itemDetailText.setHTMLFromString(detailString!)
+            
         }
         
+    }
+}
+
+
+private extension UITextView {
+    func setHTMLFromString(text: String) {
+        let modifiedFont = NSString(format:"<span style=\"font-family: \(self.font!.fontName); font-size: \(self.font!.pointSize)\">%@</span>", text) as String
+        do{
+        let attrStr = try NSAttributedString(
+            data: modifiedFont.dataUsingEncoding(NSUnicodeStringEncoding, allowLossyConversion: true)!,
+            options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute: NSUTF8StringEncoding],
+            documentAttributes: nil)
+        
+        self.attributedText = attrStr
+        }
+        catch {
+            self.text = text
+        }
     }
 }
 
