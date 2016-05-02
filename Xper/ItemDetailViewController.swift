9cf6a6a18b8eabc8fc1984d920771a78e6227597
Thomas Burguiere <thomas.burguiere@gmail.com>
2016-05-02 14:41:18 +0200

    formatting

--- Xper/ItemDetailViewController.swift
+++ Xper/ItemDetailViewController.swift
@@ -46,12 +46,12 @@ private extension UITextView {
     func setHTMLFromString(text: String) {
         let modifiedFont = NSString(format:"<span style=\"font-family: \(self.font!.fontName); font-size: \(self.font!.pointSize)\">%@</span>", text) as String
         do{
-        let attrStr = try NSAttributedString(
-            data: modifiedFont.dataUsingEncoding(NSUnicodeStringEncoding, allowLossyConversion: true)!,
-            options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute: NSUTF8StringEncoding],
-            documentAttributes: nil)
-        
-        self.attributedText = attrStr
+            let attrStr = try NSAttributedString(
+                data: modifiedFont.dataUsingEncoding(NSUnicodeStringEncoding, allowLossyConversion: true)!,
+                options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute: NSUTF8StringEncoding],
+                documentAttributes: nil)
+            
+            self.attributedText = attrStr
         }
         catch {
             self.text = text
