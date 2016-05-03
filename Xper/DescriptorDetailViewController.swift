2c5767e2155ecbf80f95f512df00275eec388239
Thomas Burguiere <thomas.burguiere@gmail.com>
2016-05-03 05:03:15 +0200

    viewing descriptor states in descriptor detail view

--- Xper/DescriptorDetailViewController.swift
+++ Xper/DescriptorDetailViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 import XperFramework
 
-class DescriptorDetailViewController: UIViewController, UINavigationControllerDelegate {
+class DescriptorDetailViewController: UIViewController, UINavigationControllerDelegate, UITableViewDataSource, UITableViewDelegate {
     var descriptor: Descriptor?
     
     @IBOutlet weak var descriptorDetailText: UITextView!
+    @IBOutlet weak var descriptorStatesTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +24,25 @@ class DescriptorDetailViewController: UIViewController, UINavigationControllerDe
                 descriptorDetailText.setHTMLFromString(detailString)
             }
         }
+        descriptorStatesTableView.dataSource = self
+        descriptorStatesTableView.delegate = self
+    }
+    
+    // MARK: - Table view data source functions
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (descriptor! as! CategoricalDescriptor).states.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("stateCell")as! DatasetObjectTableViewCell
+        let state = (descriptor! as! CategoricalDescriptor).states[indexPath.row]
+        cell.objectName?.text = state.name
         
+        return cell
     }
 }
